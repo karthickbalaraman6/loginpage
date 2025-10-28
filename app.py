ac3223eb2a902252016d4fa2a89aa396d09f1e99
karthick <karthickraja33321@gmail.com>
2025-10-28 20:49:06 +0530

    loginapp

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    if username == 'admin' and password == 'admin123':
+        return "Login Successful!"
+    else:
+        return "Invalid Credentials"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
